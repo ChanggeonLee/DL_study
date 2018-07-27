2d58c4c54bc3c23cb28170d033a030b68ef4b29c
ChanggunLee <cd8114@gmail.com>
2018-07-28 01:10:02 +0900

    chapter6

--- chapter6/mnist_ex.py
+++ chapter6/mnist_ex.py
@@ -1,20 +1,20 @@
 # mnist model
 import tensorflow as tf
 
-from tensorflow.examples.tutotials.mnist import input_data
+from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets("./mnist/data/", one_hot=True)
 
 X = tf.placeholder(tf.float32, [None, 784])
 Y = tf.placeholder(tf.float32, [None, 10])
 
 # layer
-W1 = tf.Variable(tf.random_nomal([784, 256], stddev=0.01))
+W1 = tf.Variable(tf.random_normal([784, 256], stddev=0.01))
 L1 = tf.nn.relu(tf.matmul(X,W1))
 
-W2 = tf.Variable(tf.random_nomal([256, 256], stddev=0.01))
+W2 = tf.Variable(tf.random_normal([256, 256], stddev=0.01))
 L2 = tf.nn.relu(tf.matmul(L1,W2))
 
-W3 = tf.Variable(tf.random_nomal([256, 10], stddev=0.01))
+W3 = tf.Variable(tf.random_normal([256, 10], stddev=0.01))
 model = tf.matmul(L2, W3)
 
 # optimizer
@@ -39,7 +39,7 @@ for epoch in range(15):
         _, cost_val = sess.run([optimizer, cost], feed_dict={X:batch_xs, Y:batch_ys})
         total_cost += cost_val
 
-     print('Epoch:', '%04d' % (epoch + 1), 'Avg. cost=', '{:.3f}'.format(total_cost / total_batch))
+    print ('Epoch:', '%04d' % (epoch + 1), 'Avg. cost=', '{:.3f}'.format(total_cost / total_batch))
 
 print('최적화 완료!')
 
